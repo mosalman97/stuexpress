1c1006aef7a8a5c37db771919fed7fac26ce436e
mosalman97 <mosalman1098@gmail.com>
2025-11-02 11:56:23 +0400

    params getting

--- server.js
+++ server.js
@@ -29,15 +29,19 @@ const people = [
 ];
 
 // query handling
-
 app.get("/api", (req, res) => {
 	const { age } = req.query;
 	let filterPeople = people;
 	const lastFilter = filterPeople.filter((item) => item.age >= age);
-	console.log(lastFilter, "Filter data");
 });
-
-// query
+// path params
+app.get("/api/crypto-name/:curreny", (req, res) => {
+	console.log(req.params);
+});
+// example
+app.get("/api/:category/:type", (req, res) => {
+	console.log(req.params, "NEw paramas ");
+});
 
 app.listen(PORT, () => {
 	console.log(`server is connected ${PORT}`);
