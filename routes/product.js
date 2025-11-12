827ccd0ea82f81f4b65a3152566cc741d94db049
mosalman97 <mosalman1098@gmail.com>
2025-11-12 15:47:25 +0400

    added pagenation and search option

--- routes/product.js
+++ routes/product.js
@@ -6,10 +6,12 @@ import {
 	deleteProduct,
 	editProduct,
 	productFilter,
+	searchProducts,
 } from "../controllers/product/product.js";
 
 export const productRoutes = express.Router();
 
+productRoutes.get("/filter/search", searchProducts);
 productRoutes.get("/filter", productFilter);
 productRoutes.post("/", createProduct);
 productRoutes.get("/", products);
